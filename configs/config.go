0a9eabbd58dfb86be423a7eeff52d3b32f54c52c
enocPierre <enoc.pierre@mercadolivre.com>
2022-12-13 11:17:20 -0300

    add base config

---
+++ configs/config.go
@@ -0,0 +1,43 @@
+package configs
+
+import (
+	     "github.com/go-chi/jwtauth"
+		"github.com/spf13/viper"
+	)
+
+
+
+// para carregar os confi
+var cfg *conf
+
+type conf struct {
+	DBDriver 		string `mapstruture:"DB_DRIVER"`
+	DBHost			string `mapstruture:"DB_HOST"`
+	DBPort			string `mapstruture:"DB_PORT"`
+	DBUser			string `mapstruture:"DB_USER"`
+	DBPassword		string `mapstruture:"DB_PASSWORD"`
+	DBName			string `mapstruture:"DB_NAME"`
+	WebSeverPort 	string `mapstruture:"WEB_SERVER_PORT"`
+	JWTSecret	 	string `mapstruture:"JWT_SECERET"`
+	JwtExperesIn 	int    `mapstruture:"JWT_EXPRESIN"`
+	TokenAuth		jwtauth.JWTAuth
+
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	cfg.TokenAuth = *jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg, err
+}
